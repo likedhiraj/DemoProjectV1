640bf39dc33769ae8118a1e592e1888dbbfd52dc
Dhiraj <Dhiraj@Dhiraj-PC>
2022-02-17 14:12:42 +0530

    update

---
+++ src/com/dhiraj/Test1.java
@@ -0,0 +1,13 @@
+package com.dhiraj;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+       String hungry="yes";
+       if(hungry=="Yes")
+    	   System.out.println("eat something..........");
+       else {
+		System.out.println("do wrok.........");
+	}
+	}
+}
